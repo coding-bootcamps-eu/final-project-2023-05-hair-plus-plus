4e451fddaf46966982d003c66ba71cd5b1c3546c
Nazan Bas <n-8888@t-online.de>
2023-08-15 21:09:23 +0200

    add SearchCustomerView

---
+++ src/views/SearchCustomerView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <input
+      type="search"
+      name="customers-search"
+      id="customers-search"
+      placeholder="Customers Search"
+    />
+  </div>
+  <div class="btn">
+    <button @click="searchToCustomers">Suchen</button>
+    <button @click="routeToStartPage">Zurück</button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    routeToStartPage() {
+      this.$router.push("/");
+    },
+    searchToCustomers() {
+      this.$router.push("/customercard");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.btn {
+  display: flex;
+  justify-content: center;
+  margin: 20px;
+}
+
+button {
+  padding: 8px 15px;
+  background-color: rgb(88, 91, 112);
+  color: white;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+}
+input {
+  width: 100%;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+}
+</style>
