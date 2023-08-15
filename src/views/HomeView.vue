ded6926b0c3c769ba99cc793b1185f2a7f5ae75e
Nazan Bas <n-8888@t-online.de>
2023-08-15 18:32:41 +0200

    create HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,59 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <h1>Welcome to Your Vue.js App</h1>
+  <div>
+    <header>
+      <h1>Hair++</h1>
+    </header>
+    <div class="container">
+      <button @click="routeToCustomerForm" type="button">+ new Customer</button>
+      <button @click="viewCustomers" type="button">Customers</button>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "StartPageView",
+  components: {},
+  methods: {
+    viewCustomers() {
+      this.$router.push("/customerssearch");
+      //alert("Kunden anzeigen");
+    },
+    routeToCustomerForm() {
+      this.$router.push("/customerform");
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import url(https://fonts.bunny.net/css?family=aboreto:400);
+
+header {
+  color: rgb(241, 244, 248);
+  display: flex;
+  justify-content: center;
+  margin-top: 0px;
+  padding: 110px;
+  font-size: 36px;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  gap: 150px;
+}
+button {
+  font-family: "Aboreto", display;
+  font-size: 30px;
+  background-color: rgb(88, 91, 112);
+  border-radius: 6px;
+  border: none;
+  color: white;
+  cursor: pointer;
+  transition: box-shadow 0.5s ease;
+}
+
+button:hover {
+  box-shadow: 0px 0px 10px 3px rgb(238, 226, 47);
+}
+</style>
